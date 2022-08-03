557884d917cf4244b9eeaf035fa1797eaef16a39
BeyondLogan <103182857+BeyondLogan@users.noreply.github.com>
2022-08-03 10:56:54 +0930

    Update SetUp.sh

--- SetUp.sh
+++ SetUp.sh
@@ -1,5 +1,3 @@
-curl -sLO http://packages.gameap.ru/installer.sh && bash installer.sh
-
 sudo apt-get install -y wget software-properties-common ca-certificates apt-transport-https gnupg curl lsb-release
 
 sudo wget software-properties-common ca-certificates apt-transport-https gnupg curl lsb-release
