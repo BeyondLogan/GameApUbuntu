2622087b35933efea2a9b17b2dc962a21b09b068
BeyondLogan <103182857+BeyondLogan@users.noreply.github.com>
2022-08-03 10:19:24 +0930

    Create Update2.sh

---
+++ Update2.sh
@@ -0,0 +1,15 @@
+sudo apt-get install git
+
+sudo apt-get install git-all
+
+sudo apt-get install curl
+
+sudo apt-get install wget
+
+sudo apt-get install nginx
+
+sudo apt-get install php
+
+sudo apt-get install httpd
+
+sudo reboot
