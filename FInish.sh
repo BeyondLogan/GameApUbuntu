41754a0fb75f2b473c6bdd39c3deaf3987be79c0
BeyondLogan <103182857+BeyondLogan@users.noreply.github.com>
2022-08-03 11:01:16 +0930

    Update FInish.sh

--- FInish.sh
+++ FInish.sh
@@ -1,1 +1,15 @@
+cd
+
+sudo apt-get update
+
+sudo apt update
+
+sudo apt-get upgrade
+
+sudo apt upgrade
+
+sudo apt-get update
+
+sudo apt update
+
 curl -sLO http://packages.gameap.ru/installer.sh && bash installer.sh
