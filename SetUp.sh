4eea57810b1451fe5349834b67e432d2e820fc55
BeyondLogan <103182857+BeyondLogan@users.noreply.github.com>
2022-08-03 10:40:27 +0930

    Create SetUp.sh

---
+++ SetUp.sh
@@ -0,0 +1,113 @@
+curl -sLO http://packages.gameap.ru/installer.sh && bash installer.sh
+
+sudo apt-get install -y wget software-properties-common ca-certificates apt-transport-https gnupg curl lsb-release
+
+sudo wget software-properties-common ca-certificates apt-transport-https gnupg curl lsb-release
+
+wget software-properties-common ca-certificates apt-transport-https gnupg curl lsb-release
+
+sudo apt-get install -y wget software-properties-common ca-certificates apt-transport-https gnupg curl lsb-release
+
+sudo apt-get install wget software-properties-common ca-certificates apt-transport-https gnupg curl lsb-release
+
+sudo apt-get -y install \
+    php7.3-common \
+    php7.3-cli \
+    php7.3-fpm \
+    php7.3-pdo \
+    php7.3-mysql \
+    php7.3-redis \
+    php7.3-curl \
+    php7.3-bz2 \
+    php7.3-zip \
+    php7.3-xml \
+    php7.3-mbstring \
+    php7.3-bcmath \
+    php7.3-gmp \
+    php7.3-intl
+    
+sudo apt-get install -y \
+    php7.3-common \
+    php7.3-cli \
+    php7.3-fpm \
+    php7.3-pdo \
+    php7.3-mysql \
+    php7.3-redis \
+    php7.3-curl \
+    php7.3-bz2 \
+    php7.3-zip \
+    php7.3-xml \
+    php7.3-mbstring \
+    php7.3-bcmath \
+    php7.3-gmp \
+    php7.3-intl
+    
+sudo apt-get install \
+    php7.3-common \
+    php7.3-cli \
+    php7.3-fpm \
+    php7.3-pdo \
+    php7.3-mysql \
+    php7.3-redis \
+    php7.3-curl \
+    php7.3-bz2 \
+    php7.3-zip \
+    php7.3-xml \
+    php7.3-mbstring \
+    php7.3-bcmath \
+    php7.3-gmp \
+    php7.3-intl
+    
+curl -sS https://getcomposer.org/installer | sudo php -- --install-dir=/usr/local/bin --filename=composer
+    
+sudo apt-get install mysql-server nginx
+    
+sudo apt install mysql-server
+    
+sudo mysql_secure_installation
+    
+sudo add-apt-repository universe
+    
+sudo apt install php-fpm php-mysql
+    
+sudo apt-get install mysql-server nginx
+    
+sudo apt-get install mysql-server
+    
+sudo apt-get install nginx
+    
+sudo apt-get update
+    
+sudo apt update
+    
+sudo apt-get install mariadb-server php-fpm
+    
+sudo apt-get install mariadb-server
+    
+sudo apt-get install php-fpm
+    
+sudo apt-get install groupinstall
+    
+sudo apt groupinstall
+    
+sudo apt-get groupinstall
+    
+sudo groupinstall "Development Tools"
+    
+sudo groups install "Development Tools"
+    
+sudo groupupdate "Development Tools"
+    
+sudo group update "Development Tools"
+    
+sudo groupinfo 'Development Tools'
+    
+sudo group info 'Development Tools'
+    
+groups info 'File and Print Server']
+
+clean all
+
+sudo clean all
+
+Sudo Reboot
