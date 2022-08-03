87dbd053dc735fff01951ddf174fcf35179334f1
BeyondLogan <103182857+BeyondLogan@users.noreply.github.com>
2022-08-03 10:21:08 +0930

    Create Update3.sh

---
+++ Update3.sh
@@ -0,0 +1,11 @@
+sudo apt-get install epel-release
+
+systemctl get-default
+
+sudo systemctl get-default
+
+systemctl isolate graphical.target
+
+sudo systemctl isolate graphical.target
+
+Sudo Reboot
